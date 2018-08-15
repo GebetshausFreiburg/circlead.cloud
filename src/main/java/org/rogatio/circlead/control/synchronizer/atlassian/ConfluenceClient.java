b593284d68078fa179515626cd02db6e20d7b152
matthiaw <mic.mat.wegner@web.de>
2018-08-15 13:39:55 +0200

    Add some comments to ConfluenceClient

--- src/main/java/org/rogatio/circlead/control/synchronizer/atlassian/ConfluenceClient.java
+++ src/main/java/org/rogatio/circlead/control/synchronizer/atlassian/ConfluenceClient.java
@@ -1,3 +1,11 @@
+/*
+ * Circlead - Develop and structure evolutionary Organisations
+ * 
+ * @author Matthias Wegner
+ * @version 0.1
+ * @since 01.07.2018
+ * 
+ */
 package org.rogatio.circlead.control.synchronizer.atlassian;
 
 import java.io.IOException;
@@ -8,7 +16,8 @@ import java.util.UUID;
 import org.rogatio.circlead.control.synchronizer.SynchronizerResult;
 
 /**
- * 
+ * The Class ConfluenceClient.
+ *
  * @author Matthias Wegner
  * 
  *         CLOUD
@@ -20,8 +29,18 @@ import org.rogatio.circlead.control.synchronizer.SynchronizerResult;
  */
 public class ConfluenceClient extends HttpClient {
 
+	/** The rest prefix. Is different for cloud to dedicated server */
 	private String restPrefix = null;
 
+	/**
+	 * Authentification is enabled with basic auth. Check "curl -v http://url-to-atlassian?os_authType=basic
+	 * If basic auth enabled, the "WWW-Authenticate: Basic realm="protected-area" is returned
+	 * 
+	 * @param baseUrl Url to the server, could be http or https
+	 * @param user Plain-Text of user to log in
+	 * @param password Plain text of password
+	 * @param server true if dedicated server, false if cloud server
+	 */
 	public ConfluenceClient(String baseUrl, String user, String password, boolean server) {
 		this.baseUrl = baseUrl;
 		this.user = user;
@@ -38,6 +57,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the page space.
+	 *
+	 * @param id the id
+	 * @return the page space
+	 */
 	public SynchronizerResult getPageSpace(int id) {
 		try {
 			return this.get(restPrefix + "content/" + id + "?" + "expand=space");
@@ -46,6 +71,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the page version.
+	 *
+	 * @param id the id
+	 * @return the page version
+	 */
 	public SynchronizerResult getPageVersion(int id) {
 		try {
 			return this.get(restPrefix + "content/" + id + "?" + "expand=version");
@@ -54,6 +85,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the page history.
+	 *
+	 * @param id the id
+	 * @return the page history
+	 */
 	public SynchronizerResult getPageHistory(int id) {
 		try {
 			return this.get(restPrefix + "content/" + id + "?" + "expand=history,history.lastUpdated");
@@ -62,6 +99,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the page.
+	 *
+	 * @param id the id
+	 * @return the page
+	 */
 	public SynchronizerResult getPage(int id) {
 		try {
 			return this.get(restPrefix + "content/" + id + "?" + "expand=body.storage,metadata.labels,history,history.lastUpdated,space,version,ancestors");
@@ -70,6 +113,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the page content.
+	 *
+	 * @param id the id
+	 * @return the page content
+	 */
 	public SynchronizerResult getPageContent(int id) {
 		try {
 			return this.get(restPrefix + "content/" + id + "?" + "expand=body.storage");
@@ -78,6 +127,11 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the current user.
+	 *
+	 * @return the current user
+	 */
 	public SynchronizerResult getCurrentUser() {
 		try {
 			return this.get(restPrefix + "user/current");
@@ -85,7 +139,12 @@ public class ConfluenceClient extends HttpClient {
 			return null;
 		}
 	}
-
+	
+	/**
+	 * Gets the systeminfo.
+	 *
+	 * @return the systeminfo
+	 */
 	public SynchronizerResult getSysteminfo() {
 		try {
 			return this.get(restPrefix + "settings/systemInfo");
@@ -94,6 +153,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the metadata.
+	 *
+	 * @param pageId the page id
+	 * @return the metadata
+	 */
 	public SynchronizerResult getMetadata(int pageId) {
 		try {
 			return this.get(restPrefix + "content/" + pageId + "/property");
@@ -102,10 +167,21 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Gets the rest prefix.
+	 *
+	 * @return the rest prefix
+	 */
 	public String getRestPrefix() {
 		return restPrefix;
 	}
 
+	/**
+	 * Purge page.
+	 *
+	 * @param pageId the page id
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult purgePage(int pageId) {
 		try {
 			return this.delete(restPrefix + "content/" + pageId + "?status=trashed");
@@ -115,6 +191,12 @@ public class ConfluenceClient extends HttpClient {
 		return null;
 	}
 
+	/**
+	 * Delete page.
+	 *
+	 * @param pageId the page id
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult deletePage(int pageId) {
 		try {
 			return this.delete(restPrefix + "content/" + pageId);
@@ -124,6 +206,13 @@ public class ConfluenceClient extends HttpClient {
 		return null;
 	}
 
+	/**
+	 * Delete metadata.
+	 *
+	 * @param pageId the page id
+	 * @param key the key
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult deleteMetadata(int pageId, String key) {
 		try {
 			return this.delete(restPrefix + "content/" + pageId + "/property/" + key);
@@ -133,6 +222,13 @@ public class ConfluenceClient extends HttpClient {
 		return null;
 	}
 
+	/**
+	 * Adds the label.
+	 *
+	 * @param pageId the page id
+	 * @param label the label
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult addLabel(String pageId, String label) {
 		try {
 			int id = Integer.parseInt(pageId);
@@ -143,6 +239,13 @@ public class ConfluenceClient extends HttpClient {
 		return null;
 	}
 
+	/**
+	 * Adds the label.
+	 *
+	 * @param pageId the page id
+	 * @param label the label
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult addLabel(int pageId, String label) {
 
 		String data = "{" + "\"name\" : \"" + label + "\"," + "\"prefix\": \"global\"" + "}";
@@ -157,6 +260,16 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Update metadata.
+	 *
+	 * @param pageId the page id
+	 * @param key the key
+	 * @param value the value
+	 * @param versionNumber the version number
+	 * @param minorEdit the minor edit
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult updateMetadata(int pageId, String key, String value, int versionNumber, boolean minorEdit) {
 
 		if (value.trim().startsWith("{") && value.trim().endsWith("}")) {
@@ -177,6 +290,14 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Adds the metadata.
+	 *
+	 * @param pageId the page id
+	 * @param key the key
+	 * @param value the value
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult addMetadata(int pageId, String key, String value) {
 
 		if (value.trim().startsWith("{") && value.trim().endsWith("}")) {
@@ -196,6 +317,12 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * Search.
+	 *
+	 * @param cql the cql
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult search(String cql) {
 		try {
 			String encoded = URLEncoder.encode(cql, "UTF-8");
@@ -208,8 +335,9 @@ public class ConfluenceClient extends HttpClient {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Browse content.
+	 *
+	 * @return the synchronizer result
 	 * @see https://developer.atlassian.com/cloud/confluence/rest/#api-content-get
 	 */
 	public SynchronizerResult browseContent() {
@@ -220,10 +348,23 @@ public class ConfluenceClient extends HttpClient {
 		}
 	}
 
+	/**
+	 * New page.
+	 *
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult newPage() {
 		return newPage("Page " + UUID.randomUUID(), "TEST", "<p>This is a new page</p>");
 	}
 
+	/**
+	 * New page.
+	 *
+	 * @param title the title
+	 * @param spaceKey the space key
+	 * @param content the content
+	 * @return the synchronizer result
+	 */
 	public SynchronizerResult newPage(String title, String spaceKey, String content) {
 		try {
 			String data = "{\"type\":\"page\",\"title\":\"" + title + "\",\"space\":{\"key\":\"" + spaceKey + "\"},\"body\":{\"storage\":{\"value\":\""
