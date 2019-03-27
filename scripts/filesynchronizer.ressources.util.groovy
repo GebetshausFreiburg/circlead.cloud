a71e78a47d1e7549698a3a3f489f2f5cff2cc50a
Matthias Wegner <matthias.wegner@gebetshaus-freiburg.de>
2019-03-27 19:27:19 +0100

    v1.3.2: Bugfix of null-exceptions in scripts

--- scripts/filesynchronizer.ressources.util.groovy
+++ scripts/filesynchronizer.ressources.util.groovy
@@ -2,10 +2,29 @@
 // Version: v1.0.0
 // Author: Matthias Wegner
 
-FileUtil.copyFileOrFolder(new File("data" + File.separatorChar + "ressources" + File.separatorChar + "images"),	new File("web" + File.separatorChar + "images"));
+try {
+  FileUtil.copyFileOrFolder(new File("data" + File.separatorChar + "ressources" +     File.separatorChar + "images"),	new File("web" + File.separatorChar + "images"));
+} catch (Exception e) {
+}
+
+try {
 FileUtil.copyFileOrFolder(new File("data" + File.separatorChar + "ressources" + File.separatorChar + "javascript"), new File("web" + File.separatorChar + "javascript"));
+} catch (Exception e) {
+}
+
+try {
 FileUtil.copyFileOrFolder(new File("data" + File.separatorChar + "howtos"), new File("web" + File.separatorChar + "howtos"));
+} catch (Exception e) {
+}
+
+try {
 FileUtil.copyFileOrFolder(new File("data" + File.separatorChar + "ressources" + File.separatorChar + "styles.css"), new File("web" + File.separatorChar + "styles.css"));
+} catch (Exception e) {
+}
+
+try {
 FileUtil.copyFileOrFolder(new File("data" + File.separatorChar + "ressources" + File.separatorChar + "stylesCategoryReport.css"), new File("web" + File.separatorChar + "stylesCategoryReport.css"));
+} catch (Exception e) {
+}
 
 return "sucessfully used filesynchronizer.ressources.util"
